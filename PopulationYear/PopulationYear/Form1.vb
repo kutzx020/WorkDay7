7a502073a3ddf7bfee3ecfd00bab83c42115af6f
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:57 -0500

    Work Day 7 commit.

--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,16 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
 
+
+        Dim population As Double = 7000
+        Dim year As Integer = 2014
+
+        Do Until population < 6
+            population = population / 2
+            year = year - 50
+        Loop
+
+        MessageBox.Show("The answer is " & year)
     End Sub
 End Class
